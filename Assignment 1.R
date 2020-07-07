d2f2a307e07fb3ac03d669fe5ba1264500bd6ac3
kunaldagar17 <kunal.dagar17@gmail.com>
2020-07-08 02:55:43 +0530

    Assignment 1 (EDA course)
    All the PNG files along with R file

---
+++ Assignment 1.R
@@ -0,0 +1,52 @@
+file <- "./household_power_consumption.txt"
+
+PowerData <- read.table(file, header = TRUE, sep = ";")
+powerDT <- PowerData[PowerData$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## PLOT 1
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
+
+## PLOT 2
+
+datetime <- strptime(paste(powerDT$Date,powerDT$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+
+png("plot2.png", width=480, height=480)
+plot(datetime, powerDT$Global_active_power, type="l",xlab="", ylab="Global Active Power (kilowatts)" )
+dev.off()
+
+
+## PLOT 3
+
+datetime <- strptime(paste(powerDT$Date,powerDT$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+
+png("plot3.png", width=480, height=480)
+
+plot(datetime, powerDT$Sub_metering_1, type="l",xlab="", ylab = "Energy sub metering" )
+lines(datetime, powerDT$Sub_metering_2, type="l", col = "red")
+lines(datetime, powerDT$Sub_metering_3, type="l", col = "blue")
+legend( "topright", col = c("black", "red", "blue"), c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2)
+dev.off()
+
+
+## PLOT 4
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, powerDT$Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime,powerDT$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, powerDT$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, powerDT$Sub_metering_2, type="l", col="red")
+lines(datetime, powerDT$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, powerDT$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
+
+
